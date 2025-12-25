29b8780f6cbfbaae6c27f1cc0d2ebd8063affc87
troioi-vn <chusovitin@gmail.com>
2025-12-26 02:12:07 +0700

    fix: update CORS configuration to include additional allowed origins

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -17,7 +17,15 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:8000', 'http://127.0.0.1:8000', 'http://localhost:8080'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'http://localhost:8000',
+        'http://127.0.0.1:8000',
+        'http://localhost:8080',
+        'http://localhost',
+        'http://127.0.0.1',
+    ],
 
     'allowed_origins_patterns' => [],
 
